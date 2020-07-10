3847bf2a6f08ed6e05444093294617bcbe9f8c9f
EwertonBello <ewerton.bello1212@gmail.com>
2020-07-10 16:16:01 -0300

    Starting contract and correction in the expand parameter

--- src/app/accordian/accordian.component.ts
+++ src/app/accordian/accordian.component.ts
@@ -16,25 +16,33 @@ export class AccordianComponent implements OnInit {
   @ViewChild("expandWrapper", { read: ElementRef, static: true })
   expandWrapper: ElementRef;
 
-  @Input("expanded") expanded: boolean = false;
+  @Input("expanded") expanded: boolean=false;
   @Input("expandHeight") expandHeight: string = "500px";
 
-  icon: string = "chevron-forward-outline";
-  accordionExapanded = false;
+  icon: string = this.expanded?"chevron-forward-outline":"chevron-down-outline";
+  accordionExapanded = this.expanded;
   @Input("title") title: string;
 
   constructor(public renderer: Renderer2) {}
 
   ngOnInit() {
-    this.renderer.setStyle(
-      this.expandWrapper.nativeElement,
-      "max-height",
-      this.expandHeight
-    );
+    this.accordionExapanded = String(this.expanded)!=="false"?true:false;
+    this.toggleAccordion();
   }
 
   toggleAccordion() {
     if (this.accordionExapanded) {
+      this.renderer.setStyle(
+        this.expandWrapper.nativeElement,
+        "max-height",
+        this.expandHeight
+      );
+      this.renderer.setStyle(
+        this.expandWrapper.nativeElement,
+        "padding",
+        "13px 16px"
+      );
+    } else {
       this.renderer.setStyle(
         this.expandWrapper.nativeElement,
         "max-height",
@@ -45,23 +53,9 @@ export class AccordianComponent implements OnInit {
         "padding",
         "0px 16px"
       );
-    } else {
-      this.renderer.setStyle(
-        this.expandWrapper.nativeElement,
-        "max-height",
-        "500px"
-      );
-      this.renderer.setStyle(
-        this.expandWrapper.nativeElement,
-        "padding",
-        "13px 16px"
-      );
     }
 
     this.accordionExapanded = !this.accordionExapanded;
-    this.icon =
-      this.icon == "chevron-down-outline"
-        ? "chevron-forward-outline"
-        : "chevron-down-outline";
+    this.icon = this.accordionExapanded?"chevron-forward-outline":"chevron-down-outline";
   }
 }
